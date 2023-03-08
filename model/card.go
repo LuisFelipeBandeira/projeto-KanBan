26b7330509cc44d6ac3883c55cc9ab4b0be62e0a
Luis Felipe Silva Bandeira <107070986+LuisFelipeBandeira@users.noreply.github.com>
2023-03-07 23:39:24 -0300

    .

--- model/card.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Card struct {
-	Id        int    `json:"cardid"`
-	Company   string `json:"company"`
-	Desc      string `json:"description"`
-	DateLimit string `json:"date"`
-	HourLimit string `json:"hour"`
-}
